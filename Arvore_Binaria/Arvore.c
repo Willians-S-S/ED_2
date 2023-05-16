92122c2dd1c2ecaa758fa7c6e575befe5fa26b75
Willians-S-S <09willians@gmail.com>
2023-05-15 22:18:08 -0300

    Arvore Binária

---
+++ Arvore_Binaria/Arvore.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dados{
+  int valor;
+}Dados;
+
+typedef struct arvore{
+  Dados info;
+  struct arvore *esq, *dir;
+}Arvore;
+
+Arvore *createNo(int num){
+    Arvore *New = (Arvore*) malloc(sizeof(Arvore));
+    New->info.valor = num;
+    New->esq = NULL;
+    New->dir = NULL;
+
+    return New;
+}
+
+void insert(Arvore **raiz, Arvore *no){
+    if(!(*raiz)){
+        (*raiz) = no;
+    }else if(no->info.valor < (*raiz)->info.valor){
+        insert(&((*raiz)->esq), no);
+    }else{
+        insert(&((*raiz)->dir), no);
+    }
+}
+
+void printar(Arvore *raiz){
+    if(raiz != NULL){
+        printf("%d\n", raiz->info.valor);
+        printar (raiz->esq);
+        printar (raiz->dir);
+    }
+}
+
+void search(Arvore *raiz, int num, int *encontrou, Arvore **endereco){
+    if(raiz){
+        if(raiz->info.valor == num){
+            *encontrou = 1;
+            (*endereco) = raiz;
+        }else if(num < raiz->info.valor){
+            search(raiz->esq, num, encontrou, endereco);
+        }else{
+            search(raiz->dir, num, encontrou, endereco);
+        }
+    }
+}
+
+void Remove(Arvore **raiz, int valor){
+    if((*raiz)){
+        if((*raiz)->info.valor == valor){
+            Arvore *aux = (*raiz);
+
+            if((*raiz)->esq && (*raiz)->dir){ // remove quando tem dois filhos
+                aux = (*raiz)->esq;
+
+                while(aux->dir) aux = aux->dir;
+                
+                int valor_backup = aux->info.valor;
+                aux->info.valor = (*raiz)->info.valor;
+                (*raiz)->info.valor = valor_backup;
+                
+                Remove(&((*raiz)->esq), valor);
+            }
+            else if((*raiz)->esq || (*raiz)->dir){ // remove quando tem um filho
+                if((*raiz)->esq){
+                    aux = (*raiz)->esq;
+
+                    while(aux->dir) aux = aux->dir;
+                    
+                    int valor_backup = aux->info.valor;
+                    aux->info.valor = (*raiz)->info.valor;
+                    (*raiz)->info.valor = valor_backup;
+
+                    Remove(&((*raiz)->esq), valor);
+                }else{
+                    aux = (*raiz)->dir;
+
+                    while(aux->esq) aux = aux->esq;
+                    
+                    int valor_backup = aux->info.valor;
+                    aux->info.valor = (*raiz)->info.valor;
+                    (*raiz)->info.valor = valor_backup;
+
+                    Remove(&((*raiz)->esq), valor);
+                }
+            }else{
+                free(*raiz);
+                *raiz = NULL;
+            }
+
+        }else if(valor < (*raiz)->info.valor){
+            Remove(&((*raiz)->esq), valor);
+        }else{
+            Remove(&((*raiz)->dir), valor);
+        }
+    }
+}
+
+int main(void) {
+    Arvore *raiz = NULL;
+    
+    insert(&raiz, createNo(50));
+    insert(&raiz, createNo(40));
+    insert(&raiz, createNo(60));
+    insert(&raiz, createNo(10));
+    insert(&raiz, createNo(100));
+
+    printar(raiz);
+
+    printf("\n");
+
+    // int encontrou = 0;
+    // Arvore *endereco = NULL;
+    // search(raiz, 100, &encontrou, &endereco);
+
+    // printf("Encotrou %d\n", encontrou);
+
+    // printar(endereco);
+    Remove(&raiz, 50);
+    // printar(endereco);
+    printar(raiz);
+    return 0;
+}
